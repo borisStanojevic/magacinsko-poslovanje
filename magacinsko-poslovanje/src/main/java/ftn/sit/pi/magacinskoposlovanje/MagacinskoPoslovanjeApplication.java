b012d86c4cfe00b77ee072dcecc2849f92ad616d
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2018-12-22 18:55:21 +0100

    Init

---
+++ magacinsko-poslovanje/src/main/java/ftn/sit/pi/magacinskoposlovanje/MagacinskoPoslovanjeApplication.java
@@ -0,0 +1,14 @@
+package ftn.sit.pi.magacinskoposlovanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagacinskoPoslovanjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagacinskoPoslovanjeApplication.class, args);
+	}
+
+}
+
